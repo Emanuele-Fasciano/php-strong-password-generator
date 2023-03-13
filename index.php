b9477286f441464e0ceaa795d17986fef128af6a
Emanuele-Fasciano <emanuelefasciano@gmail.com>
2023-03-13 17:25:34 +0100

    fix exercise

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!-- Milestone 3 (BONUS)
-Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite 
-$_SESSION recupererà la password da mostrare all'utente.
-Milestone 4 (BONUS)
-Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli.
- Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro 
- (es. numeri e simboli, oppure tutti e tre insieme).
-Dare all'utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali. -->
-
 <!DOCTYPE html>
 <html lang="en">
 
@@ -16,18 +7,24 @@ Dare all'utente anche la possibilità di permettere o meno la ripetizione di car
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <title>strong-password</title>
+    <title>Password generator</title>
 </head>
 
 <?php 
+
 include __DIR__ . './partials/functions.php';
+$error_message = false;
 
-$password_length = (int) $_GET["passwordLength"];
 
-if($password_length){
-    session_start();
-    $_SESSION["password"] = createPassword($password_length);
-    header("Location: ./partials/show-password.php");
+if(isset($_GET["passwordLength"])){ 
+    $password_length = (int) $_GET["passwordLength"];
+    if(!empty($password_length)){
+        session_start();
+        $_SESSION["password"] = createPassword($password_length);
+        header("Location: ./partials/show-password.php");
+    } else {
+        $error_message = true;
+    }
  };
 
 ?>
@@ -35,14 +32,13 @@ if($password_length){
 <body>
     <div class="container">
         <h1>Generatore di password</h1>
-        <form method="GET" class="my-5">
+        <form method="GET" class="mt-5">
             <label for="passwordLength">Scegliere la lunghezza della password</label> <br>
-            <input type="number" name="passwordLength" id="passwordLength" min="5" class="my-3">
+            <input type="number" name="passwordLength" id="passwordLength" min="5" class="mt-3">
             <button class="btn btn-primary">Genera</button>
         </form>
-
-        <?php if(!$password_length) : ?>
-        <h2>Scegliere la lunghezza della password</h2>
+        <?php if($error_message) : ?>
+        <h3 class="text-danger">Inserire un valore</h3>
         <?php endif ?>
     </div>
 </body>
